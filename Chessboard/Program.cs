0d3425b8c4755ff5773353f0712c9668b6d033f6
etheoo <etheoo@protonmail.com>
2023-09-21 19:24:56 +0200

    Updated comments and made minor code changes.

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -24,15 +24,7 @@ internal static class Program
 
         PrintChessBoard(boardSize, blackSquares, whiteSquares, uniquePiece, uniquePieceColumn, uniquePieceRow);
     }
-
-    /*
-     * Explanation of the method's syntax:
-     *
-     * This method is set to "private", meaning it can only be accessed within the current class.
-     * "static" is required because this method is called from another static method: Main().
-     * The datatype this method returns is an integer, hence: "int".
-     * The name of the method is "GetBoardSize", and it takes no parameters within its parentheses: "()".
-     */
+    
     private static int GetBoardSize()
     {
         const int minBoardSize = 2; // Minimum board size is 2*2 (A1-B2).
@@ -40,40 +32,28 @@ internal static class Program
 
         while (true) // Prompt user until they enter a valid board size.
         {
-            Console.WriteLine("Hur stort bräde?");
+            Console.WriteLine($"Hur stort bräde? ({minBoardSize}-{maxBoardSize})");
             var input = Console.ReadLine();
 
-            // If it's true that the string can be parsed as an int, validate that the chosen size is allowed.
+            // Validate that the user input can be parsed as int, and is within the board size.
             if (int.TryParse(input, out var size) && size is >= minBoardSize and <= maxBoardSize)
-                return size; // If the conditions are indeed true, the int is returned to the caller, exiting the loop.
-
-            // This code is only reached if the user enters an invalid size. Explicitly typing "else" is not needed in this case.
+                return size;
+            
             Console.WriteLine($"Ett fel uppstod! Värdet måste vara ett heltal mellan {minBoardSize} och {maxBoardSize}.");
         }
     }
-
-    /*
-     * Explanation of the method's syntax:
-     *
-     * This method is set to "private", meaning it can only be accessed within the current class.
-     * "static" is required because this method is called from another static method: Main().
-     * The datatype this method returns is a string, hence: "string".
-     * The name of the method is "GetUniqueString".
-     * Within the parentheses "()", we expect the caller to include a string, called "prompt" locally,
-     * as well as a List of strings, called locally "notAllowed".
-     */
+    
     private static string GetUniqueString(string prompt, List<string> notAllowed)
     {
         while (true) // Prompt user until they input a single character string that doesn't already exist in the list.
         {
-            Console.WriteLine(prompt); // The prompt will vary depending on the argument sent to this method.
+            Console.WriteLine(prompt);
             var input = Console.ReadLine();
 
-            // The input is deemed valid if the string is not (!) null, is only one character and doesn't exist in the list.
-            if (!string.IsNullOrWhiteSpace(input) && input.Length == 1 && !notAllowed.Contains(input))
+            // The user input is deemed valid if the string is not (!) null, is only one character and doesn't exist in the list.
+            if (!string.IsNullOrWhiteSpace(input) && input.Length == 1 && !notAllowed.Contains(input)) 
                 return input;
-
-            // This code is only reached if the user enters an invalid input. An appropriate error message will be printed.
+            
             if (string.IsNullOrWhiteSpace(input))
                 Console.WriteLine("Ett fel uppstod! Inmatningen får inte vara tom.");
             else if (input.Length != 1)
@@ -84,39 +64,48 @@ internal static class Program
                 Console.WriteLine("Ett okänt fel uppstod!");
         }
     }
-
-    /*
-     * Explanation of the method's syntax:
-     *
-     * This method is set to "private", meaning it can only be accessed within the current class.
-     * "static" is required because this method is called from another static method: Main().
-     * The method returns two integers: "columnCoordinate" and "rowCoordinate".
-     * The name of the method is "GetUniquePieceNotation".
-     * Within the parentheses "()", we expect the caller to include an integer, called "maxBoardSize" locally.
-     */
-    private static (int columnCoordinate, int rowCoordinate) GetUniquePieceNotation(int maxBoardSize)
+    
+    private static (int columnNotation, int rowNotation) GetUniquePieceNotation(int maxBoardSize)
     {
-        // Get the alphabetic counterpart of an int (1 = A, 2 = B, etc.)
-        var maxColumnCharacter = (char)('A' + maxBoardSize - 1);
+        /*
+         * Get the alphabetic equivalent of an int: (1 = A, 2 = B, etc.)
+         * 
+         * Typecast the decimal sum of '@' + maxBoardSize, to get the alphabetic equivalent of maxBoardSize.
+         * In the ASCII table, the char '@' is 64 in decimal, which is the decimal preceding char 'A' at 65.
+         *
+         * Example:
+         * If the integer value is "1", it is calculated as "64 + 1", is equal to 65, the decimal for char 'A'.
+         * If the integer value is "2", it is calculated as "64 + 2", is equal to 66, the decimal for char 'B'.
+         * In this context this works as is up until the decimal 90, which is the decimal for char 'Z'.
+         */
+        var maxColumnCharacter = (char)('@' + maxBoardSize);
 
         while (true) // Prompt user until they enter a valid chess notation.
         {
-            Console.WriteLine($"Var ska pjäsen stå? (mellan A1 och {maxColumnCharacter}{maxBoardSize})");
+            Console.WriteLine($"Var ska pjäsen stå? (A1-{maxColumnCharacter}{maxBoardSize})");
             var input = Console.ReadLine();
 
             if (!string.IsNullOrWhiteSpace(input))
             {
-                // Get the numeric position of the first character at index "0", in a string (A = 1, B = 2, etc.)
-                var columnNotation = char.ToUpper(input[0]) - 'A' + 1;
+                /*
+                 * Get the numeric equivalent of a char: (A = 1, B = 2, etc.)
+                 * 
+                 * The char at index 0 is converted to uppercase, before its ASCII decimal is subtracted
+                 * by the decimal of char '@', which is 64. The resulting value is then stored as an int.
+                 *
+                 * Example:
+                 * If input[0] is char 'A', 65 in decimal, it is calculated like "65 - 64", is equal to 1.
+                 * If input[0] is char 'B', 66 in decimal, it is calculated like "66 - 64", is equal to 2, and so on.
+                 */
+                var columnNotation = char.ToUpper(input[0]) - '@';
 
                 // If the string can be parsed as an int starting at index "1", validate that the chosen column and row are within the board.
                 if (int.TryParse(input[1..], out var rowNotation)
                     && columnNotation > 0 && columnNotation <= maxBoardSize
                     && rowNotation > 0 && rowNotation <= maxBoardSize)
-                    return (columnNotation, rowNotation); // return both integers.
+                    return (columnNotation, rowNotation);
             }
-
-            // This code is only reached if the user enters an invalid input.
+            
             Console.WriteLine($"Ett fel uppstod! Inmatningen \"{input}\" är en ogiltig notation.");
         }
     }
@@ -126,31 +115,30 @@ internal static class Program
     {
         for (var currentRow = 1; currentRow <= boardSize; currentRow++) // Loop through each row.
         {
-            for (var currentColumn = 1; currentColumn <= boardSize; currentColumn++)
+            for (var currentColumn = 1; currentColumn <= boardSize; currentColumn++) // Loop through each column on the current row.
             {
-                // Compare both axes in the current position against the unique piece axes.
+                // Compare the current row and column against the unique piece's row and column.
                 if (currentRow == uniquePieceRow && currentColumn == uniquePieceColumn)
                 {
-                    Console.Write(uniquePiece); // if true, print the unique piece.
+                    Console.Write(uniquePiece); // Print the unique piece, since we're on the correct notation.
                 }
                 else
                 {
                     /*
                      * How alternating output is achieved:
                      *
-                     * Since the nested (inner) for-loop's starting value will reset back to "1" for each new row,
-                     * whilst the outer for-loop continues to increment, the addition of the two integers must alternate
-                     * between even and uneven values during the start of new rows.
+                     * Since the nested/inner for-loop's starting value will reset back to "1" for each new row,
+                     * whilst the outer for-loop continues to increment, the addition of the two integers must always
+                     * alternate between even and uneven values.
                      *
                      * By using the sum of the row value and the column value against modulus (%) 2 is equal to 0,
-                     * it becomes either true or false that the result is even. It then becomes possible to use an
-                     * if-else statement to select the appropriate string to print to the console window. In this case
-                     * an ternary operator was chosen as to not create too much nesting.
+                     * it becomes either true or false that the result is even. It then becomes possible to use this
+                     * as a condition for an if-else statement in order to select the appropriate output to print to
+                     * the console window. In this case an ternary operator was chosen as to not create too much nesting.
                      */
                     Console.Write((currentRow + currentColumn) % 2 == 0 ? blackSquares : whiteSquares);
                 }
             }
-            // We reach this part of the code by printing all columns on the current row.
             Console.WriteLine(); // Make a new line for the next row.
         }
     }
